490c80101d212bfcb7bf52d819323a5bb2681df9
AnderGI <agibarguen@gmail.com>
2023-01-23 20:31:47 +0100

    Visual display on cell when its not default

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import "./styles.css";
   const computerSection = document.querySelector("section#computerGameboard");
   const userGameboard = new Gameboard();
   const computerGameboard = new Gameboard();
+  const ships = {
+    Cruise: 5,
+    Battleship: 4,
+    Submarine: 3,
+    Destroyer: 3,
+    "Patrol Boat": 2,
+  };
+
   const displayShipNameInBtn = require("./UI_Modules/displayShipNameInBtn");
   displayShipNameInBtn();
 
@@ -16,4 +24,21 @@ import "./styles.css";
   const displayGameboards = require("./UI_Modules/displayGameboards");
   displayGameboards(userGameboard.table, userSection, "userGbCell");
   displayGameboards(computerGameboard.table, computerSection, "computerGbCell");
+
+  //User Ship Visual display
+  const positionBtn = document.querySelector("button.positionBtn");
+  const shipNameBtn = document.querySelector("button.shipName");
+  const userCells = [...document.querySelectorAll("div.userGbCell")];
+ 
+
+  const displayShipShadow = (e) => {
+    const shipBtnText = shipNameBtn.textContent;
+    const positionBtnText = positionBtn.textContent;
+    if(shipBtnText !== "Place Ship" && positionBtnText !== "Alignement"){
+      const cell = e.target;
+      cell.style.backgroundColor = "white";
+    }
+  }
+
+  userCells.forEach(cell => cell.addEventListener('mouseover',displayShipShadow))
 })();
